61835f7e2ab8361311dc7cd6039b943324963f12
pandaman64 <kointosudesuyo@infoseek.jp>
2020-07-24 14:46:47 +0900

    add link to back

--- client/pages/rooms/[room_id].tsx
+++ client/pages/rooms/[room_id].tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import OekakiCanvas from '../../components/canvas'
 
@@ -8,6 +9,9 @@ export default function Room(): ReactElement {
   return (
     <>
       <div>{`room id is ${router.query.room_id}`}</div>
+      <Link href='/'>
+        <a>back to home</a>
+      </Link>
       <OekakiCanvas room_id={parseInt(router.query.room_id as string)} width={640} height={480} />
     </>
   )
